5c2edd9d3e83b7a7a405b9b344f54a0fae8f3c48
RiccardoPollice <riccardo.pollice17@gmail.com>
2023-11-29 14:10:18 +0100

    Fine progetto

--- src/org/lessons/java/security/PasswordGenerator.java
+++ src/org/lessons/java/security/PasswordGenerator.java
@@ -1,2 +1,19 @@
-package org.lessons.java.security;public class PasswordGenerator {
+package org.lessons.java.security;
+
+public class PasswordGenerator {
+    public static void main(String[] args) {
+        String name = "Riccardo";
+        String surname = "Pollice";
+        String favoriteColor = "Rosso";
+        int yearOfBirth = 1996;
+        int month = 3;
+        int day = 5;
+        int sum = 1996 + 3 + 5;
+        String password = name + " - " + surname + " - " + favoriteColor + " - " + sum;
+        System.out.println(password);
+
+
+
+
+    }
 }
